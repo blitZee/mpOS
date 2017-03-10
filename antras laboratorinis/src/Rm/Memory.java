f4a4e22f060a31d9365183768195eb9a042b9d2a
blitZee <simaitis.karolis@gmail.com>
2017-03-10 11:57:40 +0200

    Pridetas HDD ir 'load' funkcionalumas

--- antras laboratorinis/src/Rm/Memory.java
+++ antras laboratorinis/src/Rm/Memory.java
@@ -6,11 +6,29 @@ package Rm;
 public class Memory {
     byte[][][] memory;
 
-    public Memory() {
+    public Memory(Rm rm) {
         memory = new byte[256][16][4];
+        rm.ptr.data[0] = (byte) 2; //rm.ptr yra registras, jo data laukas nustatomas i 2. Tas laukas rodys i bloka,
+        //kuriame yra puslapiu lentele
     }
 
-    public void addToSupervisor(byte[][] block){
+    public void addToSupervisor(byte[][] block, boolean isCode){
+        memory[0] = block;
+
+        /*for(int i = 0; i < block.length; i++) {
+            for (int j = 0; j < block[i].length; j++)
+                System.out.print((char)block[i][j]);
+            System.out.println();
+        }*/
+    }
+
+    private void parse(boolean isCode){
+        if(isCode){
+
+        }
+        else{
+
+        }
 
     }
 }
--- antras laboratorinis/src/Rm/Rm.java
+++ antras laboratorinis/src/Rm/Rm.java
@@ -31,7 +31,7 @@ public class Rm {
 
     public Rm() {
         mode = new RmRegister(1, "mode");
-        ptr = new RmRegister(1, "ptr");
+        ptr = new RmRegister(2, "ptr");
         sp = new RmRegister(1, "sp");
         r1 = new RmRegister(4, "register 1");
         r2 = new RmRegister(4, "register 2");
@@ -43,28 +43,61 @@ public class Rm {
         sf = new RmStatusFlag();
         ic = 0;
         ti = 10;
-        memory = new Memory();
+        memory = new Memory(this);
     }
 
     public void load(String fileName, String programName){
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(fileName));
+        try (
+            BufferedReader in = new BufferedReader(new FileReader(fileName))){
             byte[][] block = new byte[16][4];
             int counter = 0;
             byte[] temp = new byte[14];
+            String buffer = in.readLine();
+            if(buffer.equals("DSEG")) {
+                while (in.ready()) {//Kol yra ka skaityti.
+                    buffer = in.readLine();
+                    System.out.println("buffer " +  buffer);
+                    if (buffer.equals("CSEG")){//Jei sutinkame CSEG, iseiname is ciklo
+                        break;
+                    }
+                    if (buffer.startsWith("DW")) {
+                        counter = addDataToBlock(block, buffer, counter); // Jei eilute prasideda 'DW', kviecieme funkcija,
+                        // kuri prideda duomenis i bloka ir grazina nauja counter reiksme, jei ji pasikeite.
 
-            while (in.ready()) {
-                String buffer = in.readLine();
-                System.out.println(buffer);
-                if (buffer.startsWith("DW")) {
-                    block[counter][0] = 'D';
-                    block[counter][1] = 'W';
-                    counter++;
-                    block[counter] = intToByte(buffer.substring(3));
-                    counter++;
+                        if (counter >= 15) { //Jei counter reiksme pasiekia bloko dydi, ji yra nunulinama ir blokas pridedamas
+                            //i supervizoriaus atminti
+                            memory.addToSupervisor(block, false);
+                            counter = 0;
+                            block = new byte[16][4];
+                        }
+                    }
                 }
             }
-            memory.addToSupervisor(block);
+            memory.addToSupervisor(block, false);
+            block = new byte[16][4];
+
+            if (buffer.equals("CSEG")){// Pabaigus DSEG, pereinam prie CSEG.
+                while (in.ready()) {//Kol yra ka skaityti.
+                    buffer = in.readLine();
+                    //System.out.println(buffer);
+                    if (buffer.equals("HALT")){//Jei sutinkame HALT, iseiname is ciklo
+                        counter = addCodeToBlock(block, buffer, counter);
+                        break;
+                    }
+                    counter = addCodeToBlock(block, buffer, counter); // Kviecieme funkcija,
+                    // kuri prideda duomenis i bloka ir grazina nauja counter reiksme, jei ji pasikeite.
+                    //validacija vyksta supervizoriaus atminty
+
+                    if (counter > 15) { //Jei counter reiksme pasiekia bloko dydi, ji yra nunulinama ir blokas pridedamas
+                        //i supervizoriaus atminti
+                        memory.addToSupervisor(block, true);
+                        counter = 0;
+                        block = new byte[16][4];
+                    }
+                }
+                memory.addToSupervisor(block, true);
+            }
+
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -72,6 +105,21 @@ public class Rm {
         catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    private int addDataToBlock(byte[][] block, String buffer, int counter){
+        block[counter][0] = 'D';
+        block[counter][1] = 'W';
+        counter++;
+        block[counter] = intToByte(buffer.substring(3));
+        counter++;
+        return counter;
+    }
+
+    private int addCodeToBlock(byte[][] block, String buffer, int counter){
+        block[counter] = buffer.getBytes();
+        counter++;
+        return counter;
     }
 
     private byte[] intToByte(String string){
