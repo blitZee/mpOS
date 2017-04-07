8eddbfb96d5236f035c1fdbdbfe5a7b4d73410fb
unknown <irmantasvar@gmail.com>
2017-04-07 12:35:59 +0300

    start of the fourth lab work

---
+++ ketvirtas laboratorinis/src/Rm/InterruptType.java
@@ -0,0 +1,14 @@
+package Rm;
+
+/**
+ * Created by blitZ on 3/8/2017.
+ */
+public enum InterruptType {
+    NO_INTERRUPT,
+    UNDEFINED_OPERATION, // PI
+    OUT_OF_MEMORY, // SI
+    INCORRECT_FILE_NAME, // SI
+    INCORRECT_FILE_HANLDE, // PI
+    DUPLICATE_NAME, // SI
+    UNDEFINED_OPERATION_WHILE_LOADING // SI
+}
